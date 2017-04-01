d2cb84270fcadf55798fa784397a2f728c6e8a24
Ginrou <gr.tnmt@gmail.com>
2017-04-02 06:56:14 +0700

    Added css

---
+++ Demostudyweb/Demostudyweb/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demostudyweb.Models
+{
+    public class Student
+    {
+        public string ID { get; set; }
+        public string firstName { get; set; }
+        public string middleName { get; set; }
+        public string lastName { get; set; }
+        public DateTime birthDay { get; set; }
+        public int gender { get; set; }
+	}
+}
