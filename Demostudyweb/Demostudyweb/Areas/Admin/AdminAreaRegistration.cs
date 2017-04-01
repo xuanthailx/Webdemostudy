5df7a9e884dc62453bd4e192e7e2271e422c3546
Ginrou <gr.tnmt@gmail.com>
2017-04-01 16:08:01 +0700

    Minor tweaks

--- Demostudyweb/Demostudyweb/Areas/Admin/AdminAreaRegistration.cs
+++ Demostudyweb/Demostudyweb/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace Demostudyweb.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "Demostudyweb.Areas.Admin.Controllers" }
             );
         }
     }
