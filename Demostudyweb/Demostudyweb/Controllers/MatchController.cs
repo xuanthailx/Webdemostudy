bb86388e0c3114def89a9045c9e788eb593e8204
Ginrou <gr.tnmt@gmail.com>
2017-04-23 17:59:31 +0700

    Show matches

---
+++ Demostudyweb/Demostudyweb/Controllers/MatchController.cs
@@ -0,0 +1,21 @@
+﻿using Model.Dow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demostudyweb.Controllers
+{
+    public class MatchController : Controller
+    {
+        private int totalRecord = 0;
+        // GET: Match
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Match index?";
+            ViewBag.Matches = new MatchDao().ListAllMatch(ref totalRecord);
+            return View();
+        }
+    }
+}
---
+++ Demostudyweb/Model/ViewModel/MatchViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+    public class MatchViewModel
+    {
+        public int ID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public TimeSpan? StartTime { get; set; }
+
+        public int? Duration { get; set; }
+
+        public string Location { get; set; }
+
+        public string Game { get; set; }
+
+        public string FirstTeam { get; set; }
+
+        public string SecondTeam { get; set; }
+
+        public int? FirstTeamScore { get; set; }
+
+        public int? SecondTeamScore { get; set; }
+
+    }
+}
