f2722a09bd7eeaaa5e219b117b8f72f424bf3cc0
DESKTOP-B82F9PQ\xuant <xuanthailxag01@gmail.com>
2017-04-26 21:41:46 +0700

    v3
    version3

---
+++ Demostudyweb/Demostudyweb/Controllers/TeamDetailController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.Dow;
+using Model.EF;
+using model.Dow;
+
+namespace Demostudyweb.Controllers
+{
+    // GET: TeamDetail
+    public class TeamDetailController : BaseController
+    {
+        // GET: Team
+        public ActionResult Index(string searchString, int? classidsearch, int page = 1, int pageSize = 5)
+        {
+
+            var dao = new TeamDetaildow();
+            var model = dao.ListAllPaging(searchString, classidsearch, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+        public ActionResult Detail(string searchString,int? id, int PageIndex = 1, int pageSize = 5)
+        {
+            var dao = new TeamDetaildow();
+            int totalRecord = 0;            
+            var model = dao.ListByID(searchString, id, ref totalRecord, PageIndex, pageSize);
+            ViewBag.SearchString = searchString;
+            ViewBag.Total = totalRecord;
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            SetViewBag();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var teamdetail = new TeamDetaildow().ViewDetail(id);
+            return View(teamdetail);
+        }
+        [HttpPost]
+        public ActionResult Create(TeamDetail teamdetail)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new TeamDetaildow();
+                long id = dao.Insert(teamdetail);
+                if (id > 0)
+                {
+                    SetAlert("Thêm chi tiết đội thành công", "success");
+                    return RedirectToAction("Index", "TeamDetail");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm đội không thành công");
+                }
+            }
+            SetViewBag();
+            return View("Index");
+        }
+        [HttpPost]
+        public ActionResult Edit(TeamDetail teamdetail)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new TeamDetaildow();
+                var result = dao.Update(teamdetail);
+                if (result)
+                {
+                    SetAlert("Sửa Chi Tiết Đội thành công", "success");
+                    return RedirectToAction("Index", "Team");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Câp nhập Chi tiết đội thành công");
+                }
+            }
+            return View("Index");
+
+        }
+        public ActionResult Delete(int id)
+        {
+            new TeamDetaildow().Delete(id);
+            return RedirectToAction("Index");
+
+        }
+        public void SetViewBag(int selectedemail = 0,int selectedteam = 0)
+        {
+
+            
+            var dao = new Studentdow();
+            ViewBag.StudentID = new SelectList(dao.ListAll(), "ID", "Email", selectedemail);
+            var dao1 = new Teamdow();
+            ViewBag.TeamID = new SelectList(dao1.ListAll(), "ID", "Name", selectedteam);
+        }
+    }
+}
--- Demostudyweb/Model/Dow/TeamDetaildow.cs
+++ Demostudyweb/Model/Dow/TeamDetaildow.cs
@@ -8,5 +8,102 @@ namespace Model.Dow
 {
     class TeamDetaildow
     {
+<<<<<<< HEAD
+=======
+        OnLinewebDbContext db = null;
+        public TeamDetaildow()
+        {
+            db = new OnLinewebDbContext();
+        }
+        public long Insert(TeamDetail entity)
+        {
+            db.TeamDetails.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+        public bool Update(TeamDetail entity)
+        {
+            try
+            {
+                var teamdetail = db.TeamDetails.Find(entity.ID);
+                teamdetail.TeamID = entity.TeamID;
+                teamdetail.StudentID = entity.StudentID;
+                teamdetail.CheckIn = entity.CheckIn;
+                //team.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public TeamDetail GetByID(int id)
+        {
+            return db.TeamDetails.SingleOrDefault(x => x.ID == id);
+
+        }
+        public TeamDetail ViewDetail(int id)
+        {
+            return db.TeamDetails.SingleOrDefault(x => x.ID == id);
+        }
+        public IEnumerable<TeamDetail> ListAllPaging(string searchString, int? classidsearch, int page, int pageSize)
+        {
+            IQueryable<TeamDetail> model = db.TeamDetails;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.TeamID.Equals(classidsearch));
+               
+            }
+            return model.OrderByDescending(x => x.ID).ToPagedList(page, pageSize);
+        }
+        public IEnumerable<TeamViewModel> ListByID(string searchString,int? id, ref int totalRecord, int PageIndex, int pageSize)
+        {
+            totalRecord = db.TeamDetails.Where(x => x.ID == id).Count();
+           var  model = from a in db.Teams                       
+                        join c in db.TeamDetails
+                        on a.ID equals c.TeamID
+                        join d in db.Students
+                        on c.StudentID equals d.ID
+                        where a.Name == searchString
+                        select new TeamViewModel()
+                        {
+                            ID = c.ID,
+                            TeamName = a.Name,
+                            StudentName = d.LastName,                            
+                            Checkin = c.CheckIn
+                        };
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.TeamName.Contains(searchString));
+
+            }
+            //.AsEnumerable().Select(x => new TeamViewModel()
+            //{
+            //    ID = x.ID,
+            //    TeamName = x.TeamName,
+            //    StudentName = x.StudentName,
+            //    GameName = x.GameName,
+            //    Checkin = x.Checkin
+            //});
+
+            return model.OrderByDescending(x=>x.ID).ToPagedList(PageIndex,pageSize);
+            
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                var teamdetail = db.TeamDetails.Find(id);
+                db.TeamDetails.Remove(teamdetail);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+>>>>>>> parent of c6d1d5d... 25/4
     }
 }
